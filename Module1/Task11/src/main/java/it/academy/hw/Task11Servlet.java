c7a274285bc79610030af79c771485e15a248a47
Andrikevich <d.andrikevich@gmail.com>
2020-12-21 13:20:55 +0300

    Created proect for Task11

---
+++ Module1/Task11/src/main/java/it/academy/hw/Task11Servlet.java
@@ -0,0 +1,18 @@
+package it.academy.hw;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet (name="task11",  urlPatterns = "/test")
+public class Task11Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final PrintWriter writer = resp.getWriter();
+        writer.println("Hello  from homework Task11 from class: " + this.getClass().getName());
+    }
+}
