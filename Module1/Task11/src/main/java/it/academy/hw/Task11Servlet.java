9895ea79a04406d1c69806e0b30a9fe0adaede21
Andrikevich <d.andrikevich@gmail.com>
2020-12-21 13:28:07 +0300

    Task11Servlet was changed and commit from anotherBranch

--- Module1/Task11/src/main/java/it/academy/hw/Task11Servlet.java
+++ Module1/Task11/src/main/java/it/academy/hw/Task11Servlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 @WebServlet (name="task11",  urlPatterns = "/test")
 public class Task11Servlet extends HttpServlet {
@@ -14,5 +15,7 @@ public class Task11Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final PrintWriter writer = resp.getWriter();
         writer.println("Hello  from homework Task11 from class: " + this.getClass().getName());
+        writer.println("-------------------");
+        writer.println(new Date());
     }
 }
